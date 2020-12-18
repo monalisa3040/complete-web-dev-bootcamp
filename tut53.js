5bdc3890f884f12b1297eca12444387bec767164
monali221 <monalisa.banerjee8991@gmail.com>
2020-12-19 05:24:12 +0530

    first commit

---
+++ tut53.js
@@ -0,0 +1,42 @@
+console.log("It is important to know ml as soon as possible");
+
+function pattern(input){
+    console.log("Hii  " + input);
+    return pattern;
+}
+
+// let input1 = "      /\"  ;
+// let input2 = "     /  \"  ;
+// let input3 = "    /    \"  ;
+// let input4 = "   /      \"  ;
+// let input5 = "  /        \"  ;
+// let input6 = " /          \"  ;
+// let input7 = "/____________\"  ;     
+
+
+let input3 = "pimona";
+let input4 = "kimona";
+let input5 = "simona";
+let input6 = "sona";
+let input7 = "mona";     
+
+
+pattern(input4);
+pattern(input5);
+pattern(input6);
+pattern(input7);
+
+
+// function drawCircle(){
+//     circle(20,20,7);
+//     color(green);
+//     return drawCircle;
+// }
+
+let age=prompt("What is your age?");
+if(age>=18){
+    alert("You are allowed in the club");
+}
+else{
+    alert("You are not allowed in the club");
+}
